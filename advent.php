55d652ef064cfe58637d7bdf01a9c3f38acb86af
Julien Verhaeghe <j.verhaeghe@attineos.com>
2018-12-03 17:34:22 +0100

    day 1 part 1

--- advent.php
+++ advent.php
@@ -1,5 +1,7 @@
 <?php
 
+$time_start = microtime(true);
+
 if (isset($argv) && isset($argv[1]) && is_numeric($argv[1])) {
     echo "------------------\n";
     echo "DAY ".$argv[1]."\n";
@@ -7,3 +9,11 @@ if (isset($argv) && isset($argv[1]) && is_numeric($argv[1])) {
 
     include(__DIR__ . DIRECTORY_SEPARATOR . "day" . $argv[1]. DIRECTORY_SEPARATOR . "solution.php");
 }
+
+$time_end = microtime(true);
+
+//dividing with 60 will give the execution time in minutes otherwise seconds
+$execution_time = ($time_end - $time_start);
+
+//execution time of the script
+echo "\nTotal Execution Time: ".number_format($execution_time * 1000, 2)."ms";
--- day1/solution.php
+++ day1/solution.php
@@ -3,12 +3,37 @@
 
     $frequency_changes  = explode("\n", $input);
 
+    // PART ONE
+
     $base_frequency     = 0;
 
     foreach ($frequency_changes as $frequency_change) {
-        $formatted_frequency = substr(trim($frequency_change), 1);
+        $formatted_frequency    = substr(trim($frequency_change), 1);
 
-        $base_frequency += $formatted_frequency * ($frequency_change[0] === "+" ? 1 : -1);
+        $base_frequency         += $formatted_frequency * ($frequency_change[0] === "+" ? 1 : -1);
     }
 
-    echo "[PART 1] : ".$base_frequency;
+    echo "[PART ONE] : ".$base_frequency;
+
+    // PART TWO
+
+    $base_frequency     = 0;
+    $frequencies_found  = [$base_frequency];
+
+    foreach ($frequency_changes as $key => $frequency_change) {
+        $formatted_frequency    = substr(trim($frequency_change), 1);
+
+        $base_frequency         += $formatted_frequency * ($frequency_change[0] === "+" ? 1 : -1);
+        if (in_array($base_frequency, $frequencies_found)) {
+
+            break;
+        } else {
+            $frequencies_found []   = $base_frequency;
+        }
+    }
+
+    sort($frequencies_found);
+    print_r($frequencies_found);
+    die();
+
+    echo "\n[PART TWO] : ".$base_frequency;
