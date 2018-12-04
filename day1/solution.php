f21a6c04aff5753f5d5752a100c33a3c041b6cfb
Julien Verhaeghe <j.verhaeghe@attineos.com>
2018-12-04 09:33:53 +0100

    [DAY1][PART1&2] Refactoring

--- day1/solution.php
+++ day1/solution.php
@@ -1,37 +1,28 @@
 <?php
-    $input              = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'input.txt');
+    $input                  = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'input.txt');
+    $frequency_changes      = explode(PHP_EOL, $input);
+    $base_frequency         = 0;
+    $frequencies_found []   = $base_frequency;
+    $part_1_solution;
+    $part_2_solution;
 
-    $frequency_changes  = explode(PHP_EOL, $input);
+    while (true) {
+        foreach ($frequency_changes as $frequency_change) {
+            $base_frequency         += (double) $frequency_change;
 
-    // PART ONE
+            if (in_array($base_frequency, $frequencies_found)) {
+                $part_2_solution = $base_frequency;
+                break 2;
+            }
 
-    $base_frequency     = 0;
-
-    foreach ($frequency_changes as $frequency_change) {
-        $formatted_frequency    = substr(trim($frequency_change), 1);
-
-        $base_frequency         += $formatted_frequency * ($frequency_change[0] === "+" ? 1 : -1);
-    }
-
-    echo "[PART ONE] : ".$base_frequency;
-
-    // PART TWO
-
-    $base_frequency     = 0;
-    $frequencies_found  = [];
-
-    foreach ($frequency_changes as $key => $frequency_change) {
-        $base_frequency         += $frequency_change;
-
-        if (in_array($base_frequency, $frequencies_found)) {
-            break;
+            $frequencies_found []   = $base_frequency;
         }
 
-        $frequencies_found []   = $base_frequency;
+        if (empty($part_1_solution)) {
+            $part_1_solution = $base_frequency;
+        }
     }
 
-    sort($frequencies_found);
-    foreach($frequencies_found as $frequency_found) {
-        echo $frequency_found."\n";
-    }
-    echo PHP_EOL . "[PART TWO] : ".$base_frequency;
+    echo "[PART ONE] : ".$part_1_solution;
+    echo PHP_EOL;
+    echo "[PART TWO] : ".$part_2_solution;
